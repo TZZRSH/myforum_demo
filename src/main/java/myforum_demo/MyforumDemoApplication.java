681a649d16f25ebeea2a666c1ea970bcd7211d64
zhaorui <2390808044@qq.com>
2023-07-06 00:09:48 +0800

    这是第一次提交

---
+++ src/main/java/myforum_demo/MyforumDemoApplication.java
@@ -0,0 +1,13 @@
+package myforum_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyforumDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyforumDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/myforum_demo/MyforumDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package myforum_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyforumDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
